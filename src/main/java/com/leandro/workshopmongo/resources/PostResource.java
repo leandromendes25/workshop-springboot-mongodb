93fc0f89b3d32e9118c5827e45beb924e8d4d5cf
leandromendes25 <leandromiendes@yahoo.com>
2020-07-27 17:06:52 -0300

    Consulta simples com query methods

--- src/main/java/com/leandro/workshopmongo/resources/PostResource.java
+++ src/main/java/com/leandro/workshopmongo/resources/PostResource.java
@@ -6,21 +6,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.leandro.workshopmongo.domain.User;
-import com.leandro.workshopmongo.dto.UserDTO;
-import com.leandro.workshopmongo.services.UserService;
+
+import com.leandro.workshopmongo.domain.Post;
+import com.leandro.workshopmongo.resources.util.URL;
+import com.leandro.workshopmongo.services.PostService;
 
 @RestController
 @RequestMapping(value = "/posts")
 public class PostResource {
 	@Autowired
-	private UserService service;
+	private PostService service;
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
-		User obj = service.FindById(id);
-		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(new UserDTO(obj));
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+ 	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = service.FindById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 }
