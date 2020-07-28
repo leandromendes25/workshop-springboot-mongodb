9a299e5f28c98b1e5b8de30383f31b5bf5b566a0
leandromendes25 <leandromiendes@yahoo.com>
2020-07-27 22:58:38 -0300

    consulta com varios criterios

--- src/main/java/com/leandro/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/leandro/workshopmongo/repository/PostRepository.java
@@ -1,8 +1,10 @@
 package com.leandro.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.leandro.workshopmongo.domain.Post;
@@ -12,4 +14,6 @@ import com.leandro.workshopmongo.domain.User;
 public interface PostRepository extends MongoRepository<Post, String>{
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	@Query(" $and: [ {date: {$gte: ?1} }, { { date: { $lte: ?2} } },{ $or: [ { 'title': {$regex: ?0, options: 'i'} }, {{ 'body': {$regex: ?0, options: 'i'}} , { 'comments.text': {$regex: ?0, options: 'i'} ] } ] }")
+	List<Post> fullSearch(String text, Date minDate,Date maxDate);
 }
